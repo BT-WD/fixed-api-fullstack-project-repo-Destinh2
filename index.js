51534186721b212941af70c67272f2f8d1b48edf
Destinh2 <destinh2@nycstudents.net>
2026-04-20 16:27:23 +0000

    Called to the Poke Api which then shows a photo of a certain pokemon onto the website

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+async function getPokemon(name) {
+        try {
+          const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+          const data = await response.json();
 
+          const imageUrl = data.sprites.other["official-artwork"].front_default;
+
+          document.getElementById("pokemon-img").src = imageUrl;
+        } catch (error) {
+        console.error("Error fetching Pokémon:", error);
+        }
+      }
+
+getPokemon("charmander");
