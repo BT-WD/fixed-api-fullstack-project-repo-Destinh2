efce674c7957f31da0bf04b02b5e497754f01a0b
Destinh2 <destinh2@nycstudents.net>
2026-04-22 16:21:33 +0000

    added the pokedex list and making the image dark so the person can see the pokemon. also added the guess feature to the code

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-async function getPokemon(name) {
-        try {
-          const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-          const data = await response.json();
+document.getElementById("reveal-answer-btn").addEventListener("click", checkAnswer);
+let currentPokemonName = "";
 
-          const imageUrl = data.sprites.other["official-artwork"].front_default;
+async function getPokemon() {
+  try {
+    const pokemonId = Math.floor(Math.random() * 1025) + 1;
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    const data = await response.json();
 
-          document.getElementById("pokemon-img").src = imageUrl;
-        } catch (error) {
-        console.error("Error fetching Pokémon:", error);
-        }
-      }
+    const img = document.getElementById("pokemon-img");
 
-getPokemon("charmander");
+    currentPokemonName = data.name.toLowerCase(); // store correct name
+
+    const imageUrl = data.sprites.other["official-artwork"].front_default;
+    img.src = imageUrl;
+    img.style.filter = "brightness(0)";
+  } catch (error) {
+    console.error("Error fetching Pokémon:", error);
+  }
+}
+
+getPokemon();
+function checkAnswer() {
+  const userInput = document.getElementById("user-input");
+  const guess = userInput.value.toLowerCase().trim();
+  const img = document.getElementById("pokemon-img");
+  if (guess === currentPokemonName) {
+    alert("Correct!");
+    img.style.filter = "brightness(1)";
+    const li = document.createElement("li");
+    li.textContent = currentPokemonName;
+    document.getElementById("pokedex-list").appendChild(li);
+    userInput.value = "";
+    setTimeout(getPokemon, 1500);
+  } else {
+    alert("Wrong! Try again.");
+  }
+}
